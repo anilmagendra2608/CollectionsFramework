46a2c377da22775b7af20f899d772f182c536c88
Anil Magendra <anil.magene@gmail.com>
2021-12-19 21:09:52 +1000

    Initial commit

--- src/ArrayListConcepts/ArrayListDemo.java
+++ src/ArrayListConcepts/ArrayListDemo.java
@@ -1,7 +1,29 @@
 package ArrayListConcepts;
 
+import java.util.ArrayList;
+
 public class ArrayListDemo {
-    public static void main(String args[]){
-        // ArrayList Implementation
-    }
+        public static void main(String[] args) {
+            arrayListExample();
+            VirtualCapacityInArrayList();
+        }
+
+        public static void arrayListExample() {
+            ArrayList<Object> ar = new ArrayList();
+            ar.add(100);
+            ar.add(200);
+            ar.add("Testing");
+            ar.add("a");
+            ar.add(15.3456D);
+            System.out.println(ar);
+            System.out.println("Physical Size of Array=" + ar.size());
+            System.out.println("Element at index 4 = " + ar.get(4));
+        }
+
+        public static void VirtualCapacityInArrayList() {
+            ArrayList<Object> ar = new ArrayList(20);
+            ar.add(300);
+            System.out.println(ar.size());
+        }
+
 }
