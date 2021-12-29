51b50ff8d2529796106d4e9dffbc96075df3fa0d
Anil Magendra <anil.magene@gmail.com>
2021-12-29 13:54:49 +1000

    Removing duplicate ele using Set

---
+++ src/ArrayListConcepts/ArrayListComparison.java
@@ -0,0 +1,10 @@
+package ArrayListConcepts;
+
+
+import java.util.Collections;
+
+public class ArrayListComparison {
+    public static void main(String args[]) {
+        Collections.emptySet();
+    }
+}
