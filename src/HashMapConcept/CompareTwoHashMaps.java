e831dcb7b4592fe81bdfd7b8dba92d1075c5210f
Anil Magendra <anil.magene@gmail.com>
2022-01-08 08:47:50 +1000

    HashMaps concepts

--- src/HashMapConcept/CompareTwoHashMaps.java
+++ src/HashMapConcept/CompareTwoHashMaps.java
@@ -1,5 +1,6 @@
 package HashMapConcept;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -41,5 +42,34 @@ public class CompareTwoHashMaps {
         CombineKeys.addAll(map4.keySet());
         CombineKeys.removeAll(map1.keySet());
         System.out.println("After Using HashSet We found Extra Key Among 2 HashMaps = " + CombineKeys);
+
+        // 4. Compare on the basis of VALUES of hashMap =
+        // WHEN DUPLICATE VALUES ARE NOT ALLOWED - Using ArrayList
+
+        HashMap<Integer, String> map5 = new HashMap<Integer, String>();
+        map5.put(1, "A");
+        map5.put(2, "B");
+        map5.put(3, "C");
+
+        HashMap<Integer, String> map6 = new HashMap<Integer, String>();
+        map6.put(4, "A");
+        map6.put(5, "B");
+        map6.put(6, "C");
+
+        HashMap<Integer, String> map7 = new HashMap<Integer, String>();
+        map7.put(1, "A");
+        map7.put(2, "B");// ignoring this as duplication not allowed in HashMap
+        map7.put(3, "C");
+        map7.put(4, "C");
+
+        System.out.println("Comparing 2 HashMaps by using equals() method ans storing only values in ArrayList = " + new ArrayList<String>(map5.values()).equals(new ArrayList<String>(map6.values()))); // TRUE
+        System.out.println("Comparing 2 HashMaps by using equals() method ans storing only values in ArrayList = " + new ArrayList<String>(map6.values()).equals(new ArrayList<String>(map7.values()))); // FALSE
+
+        // 4. Compare on the basis of VALUES of hashMap =
+        // WHEN DUPLICATE VALUES ARE ALLOWED - Using HashSet
+        System.out.println("Comparing 2 HashMaps by using equals() method ans storing only values in HashSet = " + new HashSet<>(map5.values()).equals(new HashSet<>(map6.values()))); // TRUE
+        System.out.println("Comparing 2 HashMaps by using equals() method ans storing only values in HashSet = " + new HashSet<>(map6.values()).equals(new HashSet<>(map7.values()))); // TRUE
+
+
     }
 }
